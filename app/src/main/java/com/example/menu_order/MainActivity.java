1e6c6763f2e28b4ebec8d9ab50d09610782d725f
SanaaObied <sanaobied2@gmail.com>
2024-03-10 08:31:58 -0700

    version1

---
+++ app/src/main/java/com/example/menu_order/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.menu_order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TODO Fill this Feature
+    }
+}
